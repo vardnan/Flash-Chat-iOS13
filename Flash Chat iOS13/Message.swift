4ba268ab3957694d93691d4c3c2306aae745e713
vardnan <vardnansivarajah@gmail.com>
2020-09-09 11:12:22 +0200

    initial commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Vardnan Sivarajah on 24/07/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // Contain email of the sender
+    let body: String
+    
+}
